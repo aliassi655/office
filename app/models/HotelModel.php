6bd9a167bc468637cdb42dbf5d117658d6ca5048
msmariam2078 <miss.mariam.essa1996@gmail.com>
2023-11-16 09:15:02 -0800

    add

---
+++ app/models/HotelModel.php
@@ -0,0 +1,33 @@
+<?php
+
+class RatingModel {
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+
+    public function addHotel($data) {
+        return $this->db->insert('hotels', $data);
+    }
+
+
+   
+
+    public function deleteHotel($id) {
+        $this->db->where('id', $id);
+        return $this->db->delete('hotels');
+    }
+
+   
+
+    public function getHotelById($id) {
+        return $this->db->where('id', $id)->getOne('hotels');
+    }
+    public function getHotelByCityId($id) {
+        return $this->db->where('city_id', $id)->get('hotels');
+    }
+
+}
+
